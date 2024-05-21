50c9589ac78fcae79865a267d9db566e81878f0f
namdh03 <duonghoangnam503@gmail.com>
2024-05-21 23:35:48 +0700

    refactor: change shadcn mode default to new-york

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -104,7 +104,9 @@ const FormDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
   ({ className, ...props }, ref) => {
     const { formDescriptionId } = useFormField();
 
-    return <p ref={ref} id={formDescriptionId} className={cn("text-sm text-muted-foreground", className)} {...props} />;
+    return (
+      <p ref={ref} id={formDescriptionId} className={cn("text-[0.8rem] text-muted-foreground", className)} {...props} />
+    );
   },
 );
 FormDescription.displayName = "FormDescription";
@@ -119,7 +121,12 @@ const FormMessage = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<
     }
 
     return (
-      <p ref={ref} id={formMessageId} className={cn("text-sm font-medium text-destructive", className)} {...props}>
+      <p
+        ref={ref}
+        id={formMessageId}
+        className={cn("text-[0.8rem] font-medium text-destructive", className)}
+        {...props}
+      >
         {body}
       </p>
     );
